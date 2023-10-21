1adc0e2d6b4b8bbf5fea8f2883a31d9eb351e922
HIMANSHU-1504 <himanshuprincejha2001@gmail.com>
2023-10-21 11:24:39 +0530

    Second

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 
 
 
+    implementation ("com.android.volley:volley:1.2.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.8.8")
--- app/src/main/java/com/example/medicalassistant/AdvancedDiagnosisActivity.java
+++ app/src/main/java/com/example/medicalassistant/AdvancedDiagnosisActivity.java
@@ -1,85 +1,126 @@
 package com.example.medicalassistant;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
-import com.google.gson.annotations.SerializedName;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdvancedDiagnosisActivity extends AppCompatActivity {
 
-    private EditText messageEditText;
-    private Button sendButton;
-    private TextView responseTextView;
-    private String apiKey = "sk-163YsgcYdqpL1hydEplT3BlbkFJsDhDl5Gikj155DhZxaRq"; // Replace with your OpenAI API key
-    private String apiUrl = "https://api.openai.com/v1/chat/completions";
+    private TextView responseTV;
+    private TextView questionTV;
+    private TextInputEditText queryEdt;
 
-    private Gpt3ApiService gpt3ApiService;
+    private String url = "https://api.openai.com/v1/completions";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_diagnosis);
 
-        // Initialize Retrofit
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(apiUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        responseTV = findViewById(R.id.idTVResponse);
+        questionTV = findViewById(R.id.idTVQuestion);
+        queryEdt = findViewById(R.id.idEdtQuery);
 
-        // Create the Gpt3ApiService
-        gpt3ApiService = retrofit.create(Gpt3ApiService.class);
-
-        messageEditText = findViewById(R.id.messageEditText);
-        sendButton = findViewById(R.id.sendButton);
-        responseTextView = findViewById(R.id.responseTextView);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
+        queryEdt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-                // Get the user's message
-                String userMessage = messageEditText.getText().toString();
-
-                // Create a request
-                CompletionRequest request = new CompletionRequest();
-                request.setModel("text-davinci-003");
-                request.setPrompt(Collections.singletonList(userMessage));
-
-                // Make the API call
-                Call<CompletionResponse> call = gpt3ApiService.createCompletion(request);
-                call.enqueue(new Callback<CompletionResponse>() {
-                    @Override
-                    public void onResponse(Call<CompletionResponse> call, Response<CompletionResponse> response) {
-                        if (response.isSuccessful()) {
-                            // Handle the API response
-                            CompletionResponse completionResponse = response.body();
-                            if (completionResponse != null && !completionResponse.getChoices().isEmpty()) {
-                                String text = completionResponse.getChoices().get(0).getText();
-                                responseTextView.setText(text);
-                            }
-                        } else {
-                            // Handle API error
-                            responseTextView.setText("Error: " + response.code());
-                        }
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    responseTV.setText("Please wait..");
+                    if (queryEdt.getText().toString().length() > 0) {
+                        getResponse(queryEdt.getText().toString());
+                    } else {
+                        Toast.makeText(AdvancedDiagnosisActivity.this, "Please enter your query..", Toast.LENGTH_SHORT).show();
                     }
-
-                    @Override
-                    public void onFailure(Call<CompletionResponse> call, Throwable t) {
-                        // Handle network or other errors
-                        responseTextView.setText("Error: " + t.getMessage());
-                    }
-                });
+                    return true;
+                }
+                return false;
             }
         });
+    }
+
+    private void getResponse(String query) {
+        questionTV.setText(query);
+        queryEdt.setText("");
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("model", "text-davinci-003");
+            jsonObject.put("prompt", query);
+            jsonObject.put("temperature", 0);
+            jsonObject.put("max_tokens", 100);
+            jsonObject.put("top_p", 1);
+            jsonObject.put("frequency_penalty", 0.0);
+            jsonObject.put("presence_penalty", 0.0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String responseMsg = response.getJSONArray("choices").getJSONObject(0).getString("text");
+                            responseTV.setText(responseMsg);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("TAGAPI", "Error is : " + error.getMessage() + "\n" + error);
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/json");
+                params.put("Authorization", "Bearer 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");
+                return params;
+            }
+        };
+
+        postRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+                // Retry logic here if needed
+            }
+        });
+
+        queue.add(postRequest);
     }
 }
